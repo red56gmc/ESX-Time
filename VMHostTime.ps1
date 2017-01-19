d314caaaa5e450a97e8540c337f30b3457145e82
red56gmc <red56gmc@gmail.com>
2017-01-19 09:50:31 -0800

    Update VMHostTime.ps1

--- VMHostTime.ps1
+++ VMHostTime.ps1
@@ -1,9 +1,11 @@
 #Requires -Modules VMware.VimAutomation.Core
 
-#Connect to vCenter
-$vcenter
-Connect-VIServer -Server $vcenter
+#Connect to vCenter (uncomment if not currently connected)
+#$vcenter = "vswcrpvmwvc01"
+#Connect-VIServer -Server $vcenter
 
 #Gets the current time on the system running the script and then the time for all ESXi Hosts.
-$currenttime = Get-Date -Format g; Get-View -ViewType HostSystem -Property Name,ConfigManager.DateTimeSystem | sort Name | select Name,@{Name="Current VMHost Time";Expression={(Get-View $_.ConfigManager.DateTimeSystem).QueryDateTime()}}; Get-Date -Format g
+
+$currenttime = Get-Date -Format g; Get-View -ViewType HostSystem -Property Name,ConfigManager.DateTimeSystem | sort Name | select Name,@{Name="Current VMHost Time";Expression={(Get-View $_.ConfigManager.DateTimeSystem).QueryDateTime()}}; Get-Date -Format g | Format-table -Autosize
+
 Write-Host $currenttime
