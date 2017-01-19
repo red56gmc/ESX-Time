38ec72461f271442817e9280b7ac9f3377f48ba2
red56gmc <red56gmc@gmail.com>
2017-01-19 10:00:00 -0800

    Update VMHostNTP.ps1

--- VMHostNTP.ps1
+++ VMHostNTP.ps1
@@ -1,2 +1,8 @@
+#Requires -Modules VMware.VimAutomation.Core
 
+#Connect to vCenter (uncomment if not currently connected)
+#$vcenter = "vswcrpvmwvc01"
+#Connect-VIServer -Server $vcenter
+
+#Get the currently configure NTP settings for each VMHost
 Get-VMHost | Sort-Object Name | Select Name, @{N="NTPServer";E={$_ | Get-VMHostNtpServer}}, @{N="ServiceRunning";E={(Get-VMHostService -VMHost $_ | Where-Object {$_.Key -eq "ntpd"}).Running}} | Format-Table -Autosize
