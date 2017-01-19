994213c6e62b332728f76159ac41e60ff50bae36
red56gmc <red56gmc@gmail.com>
2017-01-19 09:13:48 -0800

    Create VMHostTime.ps1

---
+++ VMHostTime.ps1
@@ -0,0 +1,5 @@
+#Requires -Modules VMware.VimAutomation.Core
+
+##Gets the current time on the system running the script and then the time for all ESXi Hosts.
+Get-Date -Format g; Get-View -ViewType HostSystem -Property Name,ConfigManager.DateTimeSystem | sort Name | select Name,@{Name="Current VMHost Time";Expression={(Get-View $_.ConfigManager.DateTimeSystem).QueryDateTime()}}; Get-Date -Format g
+
